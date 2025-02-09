f32294402c9b90e712d05b1b69d7cedf2e6a83af
SANJID0701 <sanjidtalukder07@gmail.com>
2025-02-09 11:08:48 +0600

    added pritnf in 1009.c

--- 1009.c
+++ 1009.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
 int main(){
-    char name;
-    double a, b;
-    int x=100,y=15;
+    int a=100, b=15;
+    pritnf("I am sami");
 
-   scanf("%lf %lf",&a,&b);
-   double c = (b * x)/y;
-   double d = a + c;
-   printf("TOTAL = R$ %lf\n",d);
 
 
     return 0;
